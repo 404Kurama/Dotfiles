54e025fe3d3fc45ec9dd126be17e6dfaa79e8316
404Kurama <kurama.notfound@gmail.com>
2023-02-12 12:28:27 +0700

    feat(nvim): bufferline

--- .config/nvim/lua/plugins/configs/misc/autosave.lua
+++ .config/nvim/lua/plugins/configs/misc/autosave.lua
@@ -1,5 +1,33 @@
 --// Imports
 local Autosave = require("autosave")
+local AutosaveFilters = require("autosave/filters")
 
 --// Core
-Autosave.setup({})
+Autosave.setup({
+    ["plugin"] = {
+        ["force"] = false
+    },
+    ["events"] = {
+        ["registry"] = true,
+        ["triggers"] = {
+            "InsertLeave", "TextChanged"
+        }
+    },
+    ["debounce"] = {
+        ["enabled"] = true,
+        ["delay"] = 250
+    },
+    ["filters"] = {
+        AutosaveFilters["writeable"],
+        AutosaveFilters["not_empty"],
+        AutosaveFilters["modified"]
+    },
+    ["hooks"] = {
+        ["on_enable"] = nil,
+        ["pre_filter"] = nil,
+        ["pre_write"] = function ()
+            vim.cmd("Format")
+        end,
+        ["post_write"] = nil
+    }
+})
---
+++ .config/nvim/lua/plugins/configs/ui/bufferline.lua
@@ -0,0 +1,51 @@
+--// Imports
+local Cokeline = require("cokeline")
+local Catppuccin = require("catppuccin/palettes")
+
+--// Variables
+local Colors = Catppuccin.get_palette()
+
+--// Functions
+function GetHex(Hl, G)
+	return vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID(Hl)), G)
+end
+
+--// Core
+Cokeline.setup({
+	  ["default_hl"] = {
+		    ["fg"] = function(Buffer)
+			      return Buffer["is_focused"] and GetHex("Normal", "fg") or GetHex("Comment", "fg")
+		    end,
+		    ["bg"] = GetHex("ColorColumn", "bg"),
+	  },
+    ["components"] = {
+		    {
+			      ["text"] = " ",
+			      ["bg"] = GetHex("Normal", "bg"),
+		    },
+		    {
+			      ["text"] = "",
+		        ["fg"] = GetHex("ColorColumn", "bg"),
+		        ["bg"] = GetHex("Normal", "bg"),
+        },
+        {
+            ["text"] = function (Buffer)
+                return Buffer.devicon.icon
+            end,
+            ["fg"] = function (Bufer)
+                return Bufer.devicon.color
+            end
+        },
+        {
+            ["text"] = function (Buffer)
+                return Buffer.filename
+            end,
+            ["style"] = "bold"
+        },
+		    {
+			      ["text"] = "",
+			      ["fg"] = GetHex("ColorColumn", "bg"),
+			      ["bg"] = GetHex("Normal", "bg"),
+        },
+    },
+})
